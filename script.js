0a6a2941643cf1a91872cbe8a28df77cd9f625d0
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-02-07 15:04:21 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Arquivo Javascript carregado com sucesso!')
+function calculateTip() {
+	const billAmount = parseFloat(document.getElementById('billAmount').value);
+	const tipPercentage = parseFloat(document.getElementById('tipPercentage').value);
+
+	if (isNaN(billAmount) || isNaN(tipPercentage)) {
+		alert("Pleass enter a valid number for both fields");
+		return;
+	}
+	
+
+	const tipAmount = (billAmount * tipPercentage) / 100;
+	const totalBill = billAmount + tipAmount;
+
+	document.getElementById('tipAmount').textContent = tipAmount.toFixed(2);
+	document.getElementById('totalBill').textContent = totalBill.toFixed(2);
+}
